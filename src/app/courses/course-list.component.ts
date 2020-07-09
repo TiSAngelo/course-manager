f1119b82d700e6274ef8e247b708cd365280dbd4
TiSAngelo <tisangelo@gmail.com>
2020-07-09 11:03:20 -0300

    chore: add course.service.ts.

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,3 +1,4 @@
+import { CourseService } from './course.service';
 import{Component, OnInit} from '@angular/core';
 import {Course} from './course';
 
@@ -10,28 +11,12 @@ export class CourseListComponent implements OnInit {
     
     courses: Course[] = [];
 
-    ngOnInit(): void {
-        this.courses = [
-        {
-            id: 1,
-            name: 'Angular: Forms',
-            imageUrl: '/assets/images/forms.png',
-            price: 99.99,
-            code: 'XPS-8796' ,
-            duration: 120,
-            rating: 4.5,
-            releaseDate: 'November, 2, 2019'
-        },
+    constructor(private courseService: CourseService) {
 
-        {
-            id: 2,
-            name: 'Angular: HTTP',
-            imageUrl: '/assets/images/http.png',
-            price: 45.99,
-            code: 'LKL-1094' ,
-            duration: 80,
-            rating: 4,
-            releaseDate: 'December, 4, 2019'
-        }
-    ]}
+    }
+
+    ngOnInit(): void {
+        this.courses = this.courseService.retrieveAll();
+    }
+
 }
