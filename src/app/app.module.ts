ae0e7e25034a4a537f87beacb60c1a9ec3ee416e
TiSAngelo <tisangelo@gmail.com>
2020-07-15 12:05:50 -0300

    chore: add CourseModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,27 @@
+import { CourseModule } from './courses/course.module';
 import { Error404Component } from './error-404/error404.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ReplacePipe } from './pipe/replace.pipe';
-import { StarComponent } from './star/star.component';
-import { CourseListComponent } from './courses/course-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import{FormsModule} from '@angular/forms';
 import{RouterModule} from '@angular/router';
 
 import{HttpClientModule} from '@angular/common/http';
 
-import {CourseInfoComponent} from './courses/course-info.component';
+
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
-    Error404Component,
-    CourseInfoComponent
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot([
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent 
-      },
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },      
